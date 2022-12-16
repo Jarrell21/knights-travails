d5505f35315146356fbb1c9d5a738e7e805d9d44
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-12-16 23:14:48 +0800

    Create UI interactions

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,13 +1,79 @@
 const ui = (() => {
+  const gameBoardContainer = document.querySelector('.game-board-container');
+  const placeKnightBtn = document.querySelector('.place-knight-btn');
+  const selectEndBtn = document.querySelector('.select-end-btn');
+  const resetBtn = document.querySelector('.reset-btn');
+
   const loadPage = () => {
     createGameBoard();
+    initButtons();
   };
-  const createGameBoard = () => {
-    const container = document.querySelector('.game-board-container');
 
-    for (let i = 1; i <= 64; i++) {
-      container.innerHTML += `<button>${i}</button>`;
+  const createGameBoard = () => {
+    for (let i = 1; i <= 64; i += 1) {
+      gameBoardContainer.innerHTML += `<div class="board-box" data-id="${i}"></div>`;
     }
+  };
+
+  const initButtons = () => {
+    placeKnightBtn.addEventListener('click', chooseBox);
+    resetBtn.addEventListener('click', clearGameBoard);
+  };
+
+  const chooseBox = (e) => {
+    const gameBoardBoxes = document.querySelectorAll('.board-box');
+
+    e.target.classList.add('active');
+
+    gameBoardBoxes.forEach((element) => {
+      element.style.cursor = 'pointer';
+      element.addEventListener('mouseover', changeBgColor);
+      element.addEventListener('mouseout', removeBgColor);
+      element.addEventListener('click', placeKnight);
+    });
+  };
+
+  const clearGameBoard = () => {
+    gameBoardContainer.innerHTML = '';
+    handleButtons();
+    createGameBoard();
+  };
+
+  const placeKnight = (e) => {
+    const gameBoardBoxes = document.querySelectorAll('.board-box');
+
+    gameBoardBoxes.forEach((element) => {
+      element.style.cursor = '';
+      element.removeEventListener('mouseout', removeBgColor);
+      element.removeEventListener('mouseover', changeBgColor);
+      element.removeEventListener('click', placeKnight);
+    });
+
+    e.target.textContent = 'X';
+    e.target.classList.add('main-place');
+    changeBgColor(e);
+    handleButtons();
+  };
+
+  const handleButtons = () => {
+    if (placeKnightBtn.classList.contains('active')) {
+      placeKnightBtn.classList.remove('active');
+      placeKnightBtn.setAttribute('disabled', 'true');
+      selectEndBtn.removeAttribute('disabled');
+      resetBtn.removeAttribute('disabled');
+    } else {
+      placeKnightBtn.removeAttribute('disabled');
+      selectEndBtn.setAttribute('disabled', 'true');
+      resetBtn.setAttribute('disabled', 'true');
+    }
+  };
+
+  const changeBgColor = (e) => {
+    e.target.style.background = 'red';
+  };
+
+  const removeBgColor = (e) => {
+    e.target.style.background = '';
   };
 
   return { loadPage };
