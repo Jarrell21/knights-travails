149a841430d7aff0ba9cbb32c8a2659ff2768a9b
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2022-12-25 00:29:16 +0800

    Create Graph factory and its functions

---
+++ src/modules/graph.js
@@ -0,0 +1,74 @@
+const Graph = (() => {
+  const chessBoard = new Map();
+
+  const addVertices = (size = 8) => {
+    for (let i = 1; i <= size; i += 1) {
+      for (let j = 1; j <= size; j += 1) {
+        chessBoard.set(`${[i, j]}`, []);
+      }
+    }
+  };
+
+  const addEdges = (board = chessBoard) => {
+    for (const [vertex] of board) {
+      const vertexArr = vertex.split(',');
+      const x = parseInt(vertexArr[0]);
+      const y = parseInt(vertexArr[1]);
+
+      const clockOfDirection = {
+        1: [x + 1, y + 2],
+        2: [x + 2, y + 1],
+        4: [x + 2, y - 1],
+        5: [x + 1, y - 2],
+        7: [x - 1, y - 2],
+        8: [x - 2, y - 1],
+        10: [x - 2, y + 1],
+        11: [x - 1, y + 2],
+      };
+
+      for (const pos in clockOfDirection) {
+        const move = clockOfDirection[pos].toString();
+        if (board.has(move) && !board.get(vertex).includes(move)) {
+          chessBoard.get(vertex).push(move);
+        }
+      }
+    }
+  };
+
+  const knightsTravails = (start, end) => {
+    let numberOfMoves = 0;
+    const paths = [];
+    const visited = new Set();
+    const queue = [];
+    queue.push([start, [start]]);
+
+    while (queue.length > 0) {
+      const [current, path] = queue.shift();
+      visited.add(current);
+      if (current === end) {
+        paths.push(path);
+      }
+      const neighbors = chessBoard.get(current);
+      for (const pos of neighbors) {
+        if (!visited.has(pos)) {
+          queue.push([pos, [...path, pos]]);
+        }
+      }
+    }
+
+    const moves = paths[0].map((element) => {
+      numberOfMoves += 1;
+      return element;
+    });
+    // console.log(moves);
+    // console.log(numberOfMoves);
+    return { moves, numberOfMoves };
+  };
+
+  return { addVertices, addEdges, knightsTravails };
+})();
+
+Graph.addVertices();
+Graph.addEdges();
+
+export default Graph;
